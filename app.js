674fcf599dde132915f2c094b7409a7a01547f22
ameerothman <arothman@wustl.edu>
2021-01-02 12:53:59 -0500

    response not working

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ function doSearch(){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function(){
     if (this.readyState == 4 && this.status == 200){
-      // var response = JSON.parse(this.responseText);
-      console.log(this.response.json);
+      var response = JSON.parse(this.responseText);
+      console.log(response);
 
     }
     else{
@@ -31,7 +31,7 @@ function doSearch(){
     }
   }
 
-  xhttp.open("GET", "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=15953433", true);
+  xhttp.open("GET", "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/album.get?album_id=14250417", true);
   xhttp.setRequestHeader("x-api-key", "e3ae07c70ac4d70d2402ea6bc2007a14");
   xhttp.send();
 
